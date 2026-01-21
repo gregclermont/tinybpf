dd34a0b865f95ccdd4bbea9ffd6bc56cd1be3b2a
gregclermont <580609+gregclermont@users.noreply.github.com>
2026-01-21 08:35:08 +0100

    Bump version to 0.2.0

--- src/tinybpf/__init__.py
+++ src/tinybpf/__init__.py
@@ -8,7 +8,7 @@ Example:
     ...         print(key, value)
 """
 
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 
 from tinybpf._buffers import BpfPerfBuffer, BpfRingBuffer
 from tinybpf._libbpf import init, libbpf_version
